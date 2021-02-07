96cb0315e8120835222021e63847a7cc604520b8
mcutter <mathew.utter@parsons.com>
2021-02-07 18:02:57 -0500

    Adds checks for moving and deleting files

--- tests/test_runmon.py
+++ tests/test_runmon.py
@@ -1,7 +1,8 @@
-import asyncio
+import os
 import os.path as op
+import shutil
+from runmon.display import display_debug
 
-import pytest
 from .fixtures import *
 
 
@@ -11,37 +12,58 @@ async def test_help():
     assert '--help' in p.stdout
     assert p.returncode == 0
 
+
 async def test_version():
     p = await Runmon().spawn('--version')
     await p.stop()
-    with open(get_version_file(),'r') as fd:
-      assert fd.read() == p.stdout.strip()
-
-async def test_watch_one_file(tempfiles:Tempfiles):
-
-  f1, f2 = tempfiles.make_files([
-    'test/a.txt',
-    'test/b.txt'
-  ])
-
-  p = await Runmon().spawn('-w', f1, '-e "*"', '-V', '-- rm', f2)
-  assert await p.expect(r'starting.*')
-  with open(f1, 'w') as fd:
-    fd.write('modify')
-  assert await p.expect(r'trigger.*modified')
-  await p.stop()
+    with open(get_version_file(), 'r') as fd:
+        assert fd.read() == p.stdout.strip()
 
 
-async def test_watch_one_dir(tempfiles:Tempfiles):
+async def test_watch_one_file(tempfiles: Tempfiles):
 
-  f1, f2 = tempfiles.make_files([
-    'test/a.txt',
-    'test/b.txt'
-  ])
+    f1, f2 = tempfiles.make_files([
+        'test/a.txt',
+        'test/b.txt'
+    ])
 
-  p = await Runmon().spawn('-w', tempfiles.root, '-e "*"', '-V', '-- rm', f2)
-  assert await p.expect(r'starting.*')
-  with open(f1, 'w') as fd:
-    fd.write('modify')
-  assert await p.expect(r'trigger.*modified')
-  await p.stop()
+    p = await Runmon().spawn('-w', f1, '-e "*"', '-V', '-- rm', f2)
+    assert await p.expect(r'starting.*')
+    assert await p.expect(r'clean exit.*')
+    with open(f1, 'w') as fd:
+        fd.write('modify')
+    assert await p.expect(r'trigger.*modified')
+    assert await p.expect(r'app crashed.*')
+    await p.stop()
+
+
+async def test_watch_one_dir(tempfiles: Tempfiles):
+
+    f1, f2, f3 = tempfiles.make_files([
+        'test/a.txt',
+        'test/b.txt',
+        'test/c.txt'
+    ])
+
+    p = await Runmon().spawn('-w', tempfiles.root, '-e "*"', '-V', '-- rm', f2)
+    assert await p.expect(r'starting.*')
+    assert await p.expect(r'clean exit.*')
+
+    display_debug('modifying a file')
+    with open(f1, 'w') as fd:
+        fd.write('modify')
+
+    assert await p.expect(r'trigger.*modified')
+    assert await p.expect(r'app crashed.*')
+
+    display_debug('deleting a file')
+    os.unlink(f3)
+    assert await p.expect(r'trigger.*deleted')
+    assert await p.expect(r'app crashed.*')
+
+    display_debug('moving a file')
+    shutil.move(f1, f2)
+    assert await p.expect(r'trigger.*moved')
+    assert await p.expect(r'(app crashed|clean exit).*')
+
+    await p.stop()
