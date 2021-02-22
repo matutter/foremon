2c10c3d0797226531a9066187e9410c351780837
matutter <matutter4@gmail.com>
2021-02-21 20:52:43 -0500

    Updates debouncer to use config.order

--- foremon/debounce.py
+++ foremon/debounce.py
@@ -3,29 +3,30 @@ import time
 from asyncio import BaseEventLoop
 from asyncio.events import TimerHandle
 from collections import defaultdict
-from typing import Any, Callable, DefaultDict, List, Optional
+from typing import Any, Callable, DefaultDict, List, Optional, Tuple
 
 from foremon.display import display_error, display_warning
+from foremon.task import ForemonTask
 
 
 class EventContainer:
-    set_at: int
-    args: List[Any]
+    args: Optional[Tuple[ForemonTask, Any]]
     reset_count: int
+    set_at: int
     warn_after: int
 
     def __init__(self) -> None:
         self.set_at = -1
-        self.args = []
+        self.args = None
         self.reset_count = 0
         self.warn_after = 100
 
-    def set(self, args: List[Any]):
+    def set(self, task: ForemonTask, ev: Any):
         if self.set_at != -1:
             self.reset_count += 1
 
         self.set_at = time.time()
-        self.args = args
+        self.args = (task, ev)
 
         if self.reset_count < self.warn_after:
             return
@@ -41,32 +42,39 @@ class Debounce:
     pending_events: DefaultDict
     _scheduled: Optional[TimerHandle]
 
-    def __init__(self, dwell: float, callback: Callable, loop: Optional[BaseEventLoop] = None):
+    def __init__(self,
+                 dwell: float,
+                 callback: Callable[[ForemonTask, Any], None],
+                 loop: Optional[BaseEventLoop] = None):
         self.loop = loop or asyncio.get_event_loop()
         self.callback = callback
         self.dwell = dwell
         self.pending_events = defaultdict(EventContainer)
         self._scheduled = None
 
-    def submit(self, key: str, *args: List[Any]):
+    def submit(self, task: ForemonTask, ev: Any):
         if self.dwell <= 0.0:
-            self.callback(*args)
+            self.callback(task, ev)
         else:
-            self.pending_events[key].set(args)
+            self.pending_events[task.name].set(task, ev)
             if self._scheduled:
                 self._scheduled.cancel()
             self._scheduled = self.loop.call_later(
                 self.dwell, self.drain_events)
 
-    def submit_threadsafe(self, key: str, *args: List[Any]):
-        self.loop.call_soon_threadsafe(self.submit, key, *args)
+    def submit_threadsafe(self, task: ForemonTask, ev: Any):
+        self.loop.call_soon_threadsafe(self.submit, task, ev)
 
     def drain_events(self):
         self._scheduled = None
         containers = list(self.pending_events.values())
         self.pending_events.clear()
 
-        for cont in containers:
+        def get_order(cont: EventContainer):
+            return cont.args[0].config.order
+
+        cont: EventContainer
+        for cont in sorted(containers, key=get_order):
             try:
                 self.callback(*cont.args)
             except Exception as e:
--- tests/test_debounce.py
+++ tests/test_debounce.py
@@ -1,68 +1,91 @@
 import asyncio
+from foremon.task import ForemonTask
+from typing import Callable
 
 from foremon.debounce import Debounce, EventContainer
-
+from pytest_mock.plugin import MockerFixture
+from itertools import count
 from .fixtures import *
 
 
-async def test_debounce_dwell_submit():
+@pytest.fixture
+def MockTask(mocker: MockerFixture):
+    incr = count(start=0)
+    def MockFactory(name: str, order = None):
+        inc = next(incr)
+        if order is None:
+            order = inc
+
+        c = mocker.MagicMock()
+        mocker.patch.object(c, 'order', order)
+
+        o = mocker.MagicMock()
+        mocker.patch.object(o, 'name', name)
+        mocker.patch.object(o, 'config', c)
+
+        return o
+
+    return MockFactory
+
+async def test_debounce_dwell_submit(MockTask:Callable[[str], ForemonTask]):
     result = []
     dwell = 0.1
-    d = Debounce(dwell, result.append)
-    d.submit('x', 'x')
-    d.submit('y', 'y')
+    d = Debounce(dwell, lambda *args: result.append(args[1]))
+    d.submit(MockTask('x'), 'x')
+    d.submit(MockTask('y'), 'y')
     assert not result
     await asyncio.sleep(dwell)
     assert 'x' in result
     assert 'y' in result
 
 
-async def test_debounce_no_dwell_submit():
+async def test_debounce_no_dwell_submit(MockTask):
     result = []
     dwell = -0.1
-    d = Debounce(dwell, result.append)
-    d.submit('x', 'x')
-    d.submit('y', 'y')
+    d = Debounce(dwell, lambda *args: result.append(args[1]))
+    d.submit(MockTask('x'), 'x')
+    d.submit(MockTask('y'), 'y')
     assert 'x' in result
     assert 'y' in result
 
 
-async def test_debounce_no_dwell_submit_threadsafe():
+async def test_debounce_no_dwell_submit_threadsafe(MockTask):
     result = []
     dwell = -0.1
-    d = Debounce(dwell, result.append)
-    d.submit_threadsafe('x', 'x')
-    d.submit_threadsafe('y', 'y')
+    d = Debounce(dwell, lambda *args: result.append(args[1]))
+    d.submit_threadsafe(MockTask('x'), 'x')
+    d.submit_threadsafe(MockTask('y'), 'y')
     assert 'x' not in result
     assert 'y' not in result
     await asyncio.sleep(0.1)
     assert 'x' in result
     assert 'y' in result
 
-async def test_debounce_submit_throws(output: CapLines):
+
+async def test_debounce_submit_throws(output: CapLines, MockTask):
     def thrower(*args):
         raise Exception('test')
     d = Debounce(0.1, thrower)
-    d.submit('a', 'a')
+    d.submit(MockTask('a'), 'a')
     await asyncio.sleep(0.1)
     assert output.stderr_expect('drain callback error.*')
 
 
-async def test_debounce_no_dwell_submit_throws(output: CapLines):
+async def test_debounce_no_dwell_submit_throws(output: CapLines, MockTask):
     def thrower(*args):
         raise Exception('test')
     d = Debounce(0.0, thrower)
     with pytest.raises(Exception):
-        d.submit('a', 'a')
+        d.submit(MockTask('a'), 'a')
 
 
-async def test_debounce_dwell_submit(output: CapLines):
+async def test_debounce_dwell_high_volume_submit(output: CapLines, MockTask):
     result = []
     dwell = 0.1
-    d = Debounce(dwell, result.append)
+    d = Debounce(dwell, lambda *args: result.append(args[1]))
     for i in range(0, 400):
-        d.submit('x', 'x')
-        d.submit('y', 'y')
+        d.submit(MockTask('x'), 'x')
+        d.submit(MockTask('y'), 'y')
     assert not result
     await asyncio.sleep(dwell)
     assert 'x' in result
